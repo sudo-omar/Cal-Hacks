9714beede1879119e45ca96831bfdf8f703c72ee
Blade6264 <104872046+Blade6264@users.noreply.github.com>
2024-10-19 09:52:15 -0700

    Added login and signup pages

--- cal-hacks/src/App.js
+++ cal-hacks/src/App.js
@@ -3,8 +3,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Change to use Routes
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+// import About from './components/About';
+// import Contact from './components/Contact';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
 import HealthHub from './components/HealthHub';
 import LogOfCases from './components/LogOfCases';
 import ReportAndTalk from './components/ReportAndTalk';
@@ -18,7 +20,8 @@ const App = () => {
         <Route path="/" element={<Home />} /> {/* Use element prop instead of component */}
         <Route path="/log-of-cases" element={<LogOfCases />} />
         <Route path="/report-and-talk" element={<ReportAndTalk />} />
-
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         {/*health hub aka the main recording page */}
         <Route path="/healthhub" element={<HealthHub />} />
       </Routes>
---
+++ cal-hacks/src/components/SignUp.jsx
@@ -0,0 +1,9 @@
+const SignUp = () => {
+    return (
+        <div>
+            logofcases
+        </div>
+    )
+}
+
+export default SignUp;
