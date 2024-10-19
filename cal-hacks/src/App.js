a153f747f6e5ea9b8844588629305d4d468883ba
sudo-omar <omarabouharb2805@gmail.com>
2024-10-19 11:34:51 -0700

    f

--- cal-hacks/src/App.js
+++ cal-hacks/src/App.js
@@ -3,9 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Change to use Routes
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import SignUp from './components/SignUp';
-import Login from './components/Login';
-import HealthHub from './components/HealthHub';
 import About from './components/About';
 import Contact from './components/Contact';
 import LogOfCases from './components/LogOfCases';
@@ -25,18 +22,12 @@ const App = () => {
         <Route path="/" element={<Home />} /> {/* Use element prop instead of component */}
         <Route path="/log-of-cases" element={<LogOfCases />} />
         <Route path="/report-and-talk" element={<ReportAndTalk />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        {/*health hub aka the main recording page */}
-        <Route path="/healthhub" element={<HealthHub />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-<<<<<<< HEAD
         <Route path="/appointments/1" element={<FirstAppointment />} />
         <Route path="/appointments/2" element={<SecondAppointment />} />
         <Route path="/appointments/3" element={<ThirdAppointment />} />
-=======
->>>>>>> 0065cc137ce6394756749db8ae4aad14831b50b2
+        <Route path="/appointments/3" element={<ThirdAppointment />} />
       </Routes>
     </Router>
 
